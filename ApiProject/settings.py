0d0daa020be896cf5a2fc9bf48db1346c2e3e497
Ng-Harry <ngereharry5@gmail.com>
2022-10-29 15:32:23 +0100

    update Procfile

--- ApiProject/settings.py
+++ ApiProject/settings.py
@@ -122,3 +122,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configure Django App for Heroku
+import django_on_heroku
+django_on_heroku.settings(locals())
