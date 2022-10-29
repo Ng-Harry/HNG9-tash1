2c08baf791678badc047961b344b3f93f8879046
Ng-Harry <ngereharry5@gmail.com>
2022-10-29 21:39:05 +0100

    rename username field

--- slackuser/models.py
+++ slackuser/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class SlackUser(models.Model):
-    username = models.CharField(max_length=150, unique=True, verbose_name="slack username")
+    slack_username = models.CharField(max_length=150, unique=True, verbose_name="slack username")
     backend = models.BooleanField(default=False)
     age = models.IntegerField()
     bio = models.TextField()
